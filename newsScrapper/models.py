fd10253f6d94b6c7be028a5088fa3b6d0384424d
ahmadkhalil97 <ahmadkhalil.bk97@gmail.com>
2020-03-01 19:53:08 +0500

    before model changing

--- newsScrapper/models.py
+++ newsScrapper/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
+# class Source(models.Model):
+#     source_name = models.CharField(max_length=100)
+#     source_link = models.TextField()
+
+
 class Headlines(models.Model):
+    # source = models.ForeignKey('newsScrapper.Source', related_name='news', on_delete=models.CASCADE())
     title = models.CharField(max_length=250)
     link = models.TextField()
+
+    # category = models.CharField(max_length=100, blank=True, null=True)
+    # published_date = models.DateField(default=timezone.now().date())
 
     def __str__(self):
         return self.title
--- newsScrapper/views.py
+++ newsScrapper/views.py
@@ -13,14 +13,14 @@ requests.packages.urllib3.disable_warnings()
 
 def scrap_all(request):
     news_all = [scrap_bbc(), scrap_new_york_times(), scrap_los_angles_times(), scrap_wired(), scrap_the_onion(),
-                scrap_cnn()]
+                scrap_cnn(), scrap_quartz(), scrap_washington_post(), scrap_the_verge()]
     return render(request, 'newsScrapper/news.html', {'news_all': news_all})
 
 
 def scrap_the_onion():
     session = requests.session()
     session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
-    url = 'https://www.theonion.com/'
+    url = 'https://www.theonion.com'
     content = session.get(url, verify=False).content
 
     soup = BeautifulSoup(content, 'lxml', from_encoding="utf8")
@@ -28,15 +28,16 @@ def scrap_the_onion():
     articles = soup.find_all('article')
     news_list = []
     for i in articles:
-        title = i.find('h4').text
-        link = i.find_all('a')[-1]['href']
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('h4') and i.find('a'):
+            title = i.find('h4').text.strip
+            link = i.find_all('a')[-1]['href']
+            news_list.append(Headlines(title=title, link=link))
 
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'The-Onion', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
@@ -54,16 +55,17 @@ def scrap_new_york_times():
     articles = soup.find_all('article')
     news_list = []
     for i in articles:
-        title = i.find_all('h2')[-1].text
-        link = i.find_all('a')[0]['href']
-        if not link.startswith(url):
-            link = url + link
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('h2') and i.find('a'):
+            title = i.find_all('h2')[-1].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'NY-Times', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
@@ -81,16 +83,17 @@ def scrap_wired():
     rows = soup.select('.primary-grid-component .cards-component__row > div')
     news_list = []
     for i in rows:
-        title = i.find('h2').text
-        link = i.find_all('a')[0]['href']
-        if not link.startswith(url):
-            link = url + link
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('h2') and i.find('a'):
+            title = i.find('h2').text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'Wired', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
@@ -108,16 +111,17 @@ def scrap_bbc():
     rows = soup.select('.module--promo h3')
     news_list = []
     for i in rows:
-        title = i.find_all('a')[0].text.replace('"', '').strip()
-        link = i.find_all('a')[0]['href']
-        if not link.startswith(url):
-            link = url + link
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('a'):
+            title = i.find_all('a')[0].text.replace('"', '').strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'BBC', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
@@ -135,16 +139,17 @@ def scrap_los_angles_times():
     rows = soup.select('.ListAA-items-item')
     news_list = []
     for i in rows:
-        title = i.find_all('a')[1].text.replace('"', '').strip()
-        link = i.find_all('a')[1]['href']
-        if not link.startswith(url):
-            link = url + link
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('a'):
+            title = i.find_all('a')[1].text.replace('"', '').strip
+            link = i.find_all('a')[1]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'LA-Times', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
@@ -165,20 +170,115 @@ def scrap_cnn():
     # print(rows)
     news_list = []
     for i in rows:
-        title = i.find_all('span', {'class': 'cd__headline-text'})[0].text
-        link = i.find_all('a')[0]['href']
-        if not link.startswith(url):
-            link = url + link
-        news_list.append(Headlines(title=title, link=link))
+        if i.find('span', {'class': 'cd__headline-text'}) and i.find('a'):
+            title = i.find_all('span', {'class': 'cd__headline-text'})[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
     news_count = len(news_list)
     if news_count > 5:
         top_news = []
         for i in range(5):
-            top_news.append(news_list.pop(i))
+            top_news.append(news_list.pop(0))
         return {'source': 'CNN', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
                 'news_list': news_list}
     else:
         return {'source': 'CNN', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_quartz():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://qz.com'
+    req_url = 'https://qz.com/latest/'
+    content = session.get(req_url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('article')
+    news_list = []
+    for i in rows:
+        if i.find('h3') and i.find('a'):
+            title = i.find_all('h3')[-1].text.strip
+            link = i.find_all('a')[-1]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'QUARTZ', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'QUARTZ', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_washington_post():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.washingtonpost.com'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('#main-content .top-table-col > div')
+    a_all = []
+    news_list = []
+    for i in rows:
+        if i.find('ul', {'class': 'related-links'}):
+            for j in i.select('.related-links li'):
+                if j.find('a'):
+                    title = j.find_all('a')[0].text.strip
+                    link = j.find_all('a')[0]['href']
+                    if not link.startswith(url):
+                        link = url + link
+                    news_list.append(Headlines(title=title, link=link))
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'Washington-Post', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'Washington-Post', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_the_verge():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.theverge.com'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('.c-compact-river__entry .c-entry-box--compact__body')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'The-Verge', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'The-Verge', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
 
 # def get_twitter_trends():
 #     consumer_key = 'secret'
