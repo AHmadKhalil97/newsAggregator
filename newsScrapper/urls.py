663f26668d27b761a9a645dc8691b12f0c5bc764
ahmadkhalil97 <ahmadkhalil.bk97@gmail.com>
2020-03-03 16:59:05 +0500

    categories_added

--- newsScrapper/urls.py
+++ newsScrapper/urls.py
@@ -3,5 +3,8 @@ from newsScrapper import views
 
 urlpatterns = [
     # path('', views.scrap_the_onion, name='scrapTheOnion'),
-    path('', views.scrap_all, name='scrapAll'),
+    path('', views.scrap_all_latest, name='scrap_all_latest'),
+    path('politics', views.scrap_all_politics, name='scrap_all_politics'),
+    path('business', views.scrap_all_business, name='scrap_all_business'),
+    path('health', views.scrap_all_health, name='scrap_all_health'),
 ]
--- newsScrapper/views.py
+++ newsScrapper/views.py
@@ -11,9 +11,24 @@ requests.packages.urllib3.disable_warnings()
 
 # Create your views here.
 
-def scrap_all(request):
+def scrap_all_latest(request):
     news_all = [scrap_bbc(), scrap_new_york_times(), scrap_los_angles_times(), scrap_wired(), scrap_the_onion(),
                 scrap_cnn(), scrap_quartz(), scrap_washington_post(), scrap_the_verge()]
+    return render(request, 'newsScrapper/news.html', {'news_all': news_all})
+
+
+def scrap_all_politics(request):
+    news_all = [scrap_ny_times_politics(), scrap_washington_post_politics(), scrap_wsj_washington_wire_politics()]
+    return render(request, 'newsScrapper/news.html', {'news_all': news_all})
+
+
+def scrap_all_business(request):
+    news_all = [scrap_wsj_washington_wire_business(), scrap_ny_times_business(), scrap_business_insider()]
+    return render(request, 'newsScrapper/news.html', {'news_all': news_all})
+
+
+def scrap_all_health(request):
+    news_all = [scrap_harvard_health(), scrap_webmd(), scrap_wellness_mama()]
     return render(request, 'newsScrapper/news.html', {'news_all': news_all})
 
 
@@ -225,7 +240,6 @@ def scrap_washington_post():
     soup = BeautifulSoup(content, 'lxml')
 
     rows = soup.select('#main-content .top-table-col > div')
-    a_all = []
     news_list = []
     for i in rows:
         if i.find('ul', {'class': 'related-links'}):
@@ -236,7 +250,15 @@ def scrap_washington_post():
                     if not link.startswith(url):
                         link = url + link
                     news_list.append(Headlines(title=title, link=link))
-        if i.find('a'):
+        if i.find('ul', {'class': 'timeline-stream'}):
+            for j in i.select('.timeline-stream li'):
+                if j.find('a'):
+                    title = j.find_all('a')[0].text.strip
+                    link = j.find_all('a')[0]['href']
+                    if not link.startswith(url):
+                        link = url + link
+                    news_list.append(Headlines(title=title, link=link))
+        if i.find('a') and i.find('a').has_attr('href'):
             title = i.find_all('a')[0].text.strip
             link = i.find_all('a')[0]['href']
             if not link.startswith(url):
@@ -279,6 +301,278 @@ def scrap_the_verge():
                 'news_list': news_list}
     else:
         return {'source': 'The-Verge', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_ny_times_politics():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.nytimes.com'
+    req_url = 'https://www.nytimes.com/section/politics'
+    content = session.get(req_url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('.css-f8yl15 article h2')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'NY-Times', 'source_url': req_url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'NY-Times', 'source_url': req_url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_washington_post_politics():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.washingtonpost.com'
+    req_url = 'https://www.washingtonpost.com/politics'
+    content = session.get(req_url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('#main-content .story-list-story h2')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'Washington-Post', 'source_url': req_url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'Washington-Post', 'source_url': req_url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_wsj_washington_wire_politics():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.wsj.com/news/types/washington-wire'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('article')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            # if not link.startswith(url):
+            #     link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'WSJ-Washington-Wire', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'WSJ-Washington-Wire', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_wsj_washington_wire_business():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.wsj.com/news/business'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('article')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            # if not link.startswith(url):
+            #     link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'WSJ-Business', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'WSJ-Business', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_ny_times_business():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.nytimes.com'
+    req_url = 'https://www.nytimes.com/section/business'
+    content = session.get(req_url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('.ekkqrpp3 article h2')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'NY-Times', 'source_url': req_url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'NY-Times', 'source_url': req_url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_business_insider():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.businessinsider.com'
+    req_url = 'https://www.businessinsider.com/clusterstock'
+    content = session.get(req_url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.select('section h2')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'Business-Insider', 'source_url': req_url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'Business-Insider', 'source_url': req_url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_harvard_health():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.health.harvard.edu'
+    # req_url = 'https://www.businessinsider.com/clusterstock'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+
+    rows = soup.find_all('article',
+                         {'class': ['article-listing-item', 'blog-listing-item', 'document-carousel-text-block']})
+    a_all = []
+    for i in rows:
+        if i.find('h2'):
+            a_all += i.select('h2 a')
+        elif i.find('h3'):
+            a_all += i.select('h3 a')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'Harvard-Health', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'Harvard-Health', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_webmd():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://www.webmd.com'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+    rows = soup.select('.topStories a')
+    news_list = []
+
+    hero_story_title = soup.select('.hero-with-caption .caption h2')[0].text.strip
+    hero_story_link = soup.select('.hero-with-caption > a')[0]['href']
+    news_list.append(Headlines(title=hero_story_title, link=hero_story_link))
+    for i in rows:
+        if i.find('h3'):
+            title = i.find_all('h3')[0].text.strip
+            link = i['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+        elif i.find('h2'):
+            title = i.find_all('h2')[0].text.strip
+            link = i['href']
+            if not link.startswith(url):
+                link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'WebMd', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'WebMd', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
+
+
+def scrap_wellness_mama():
+    session = requests.session()
+    session.headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36'
+    url = 'https://wellnessmama.com/blog/'
+    content = session.get(url, verify=False).content
+
+    soup = BeautifulSoup(content, 'lxml')
+    rows = soup.select('article h2')
+    news_list = []
+    for i in rows:
+        if i.find('a'):
+            title = i.find_all('a')[0].text.strip
+            link = i.find_all('a')[0]['href']
+            # if not link.startswith(url):
+            #     link = url + link
+            news_list.append(Headlines(title=title, link=link))
+    news_count = len(news_list)
+    if news_count > 5:
+        top_news = []
+        for i in range(5):
+            top_news.append(news_list.pop(0))
+        return {'source': 'Wellness-Mama', 'source_url': url, 'news_count': news_count, 'top_news': top_news,
+                'news_list': news_list}
+    else:
+        return {'source': 'Wellness-Mama', 'source_url': url, 'news_count': news_count, 'news_list': news_list}
 
 # def get_twitter_trends():
 #     consumer_key = 'secret'
